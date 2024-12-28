62d121050664e2ffed03d587de2ddf40d7e8d00b
Jerzy Boksa <jerzyxboksa@gmail.com>
2024-12-28 22:10:36 +0100

    fix liquibase tests

--- shopland-be/src/main/resources/db/create_tables.sql
+++ shopland-be/src/main/resources/db/create_tables.sql
@@ -1,16 +1,14 @@
-CREATE TYPE user_role AS ENUM ('ADMIN', 'USER');
-
-CREATE TABLE IF NOT EXISTS users
+CREATE TABLE app_users
 (
     id       SERIAL PRIMARY KEY,
     email    VARCHAR(256) NOT NULL UNIQUE,
     name     VARCHAR(256) NOT NULL,
     surname  VARCHAR(256) NOT NULL,
     password VARCHAR(256) NOT NULL,
-    role     user_role
+    role     VARCHAR(20)  NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS products
+CREATE TABLE products
 (
     id                SERIAL PRIMARY KEY,
     name              VARCHAR(256) NOT NULL,
@@ -21,36 +19,36 @@ CREATE TABLE IF NOT EXISTS products
     image             BYTEA
 );
 
-CREATE TABLE IF NOT EXISTS carts
+CREATE TABLE carts
 (
     user_id    INTEGER NOT NULL,
     product_id INTEGER NOT NULL,
     quantity   INTEGER CHECK (quantity > 0),
-    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
-    CONSTRAINT fk_product FOREIGN KEY (product_id) REFERENCES products (id) ON DELETE CASCADE,
+    CONSTRAINT fk_user_carts FOREIGN KEY (user_id) REFERENCES app_users (id) ON DELETE CASCADE,
+    CONSTRAINT fk_product_carts FOREIGN KEY (product_id) REFERENCES products (id) ON DELETE CASCADE,
     CONSTRAINT unique_user_product UNIQUE (user_id, product_id),
     PRIMARY KEY (user_id, product_id)
 );
 
-CREATE TABLE IF NOT EXISTS orders
+CREATE TABLE orders
 (
     id            SERIAL PRIMARY KEY,
     user_id       INTEGER   NOT NULL,
     date          TIMESTAMP NOT NULL,
     summary_price INTEGER CHECK ( summary_price >= 0),
-    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+    CONSTRAINT fk_user_orders FOREIGN KEY (user_id) REFERENCES app_users (id) ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS order_products
+CREATE TABLE order_products
 (
     order_id   INTEGER NOT NULL,
     product_id INTEGER NOT NULL,
     CONSTRAINT fk_order FOREIGN KEY (order_id) REFERENCES orders (id) ON DELETE CASCADE,
-    CONSTRAINT fk_product FOREIGN KEY (product_id) REFERENCES products (id) ON DELETE CASCADE,
+    CONSTRAINT fk_product_orders FOREIGN KEY (product_id) REFERENCES products (id) ON DELETE CASCADE,
     PRIMARY KEY (order_id, product_id)
 );
 
-CREATE TABLE IF NOT EXISTS opinions
+CREATE TABLE opinions
 (
     id         SERIAL PRIMARY KEY,
     product_id INTEGER NOT NULL,
@@ -58,10 +56,10 @@ CREATE TABLE IF NOT EXISTS opinions
     value      INTEGER CHECK (value >= 1 AND value <= 5),
     content    VARCHAR(512),
     CONSTRAINT unique_product_user UNIQUE (product_id, user_id),
-    CONSTRAINT fk_product FOREIGN KEY (product_id) REFERENCES products (id) ON DELETE CASCADE,
-    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+    CONSTRAINT fk_product_opinions FOREIGN KEY (product_id) REFERENCES products (id) ON DELETE CASCADE,
+    CONSTRAINT fk_user_opinions FOREIGN KEY (user_id) REFERENCES app_users (id) ON DELETE CASCADE
 );
 
 CREATE INDEX idx_carts_user_id ON carts (user_id);
 CREATE INDEX idx_order_products_order_id ON order_products (order_id);
-CREATE INDEX idx_users_email ON users (email);
+CREATE INDEX idx_users_email ON app_users (email);
