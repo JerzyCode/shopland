2d549cc6a8153edfea436b45323ff3957c7eab7b
Mateusz <mateusz.oracz2001@gmail.com>
2025-01-10 12:07:54 +0100

    basic view of home page

--- shopland-fe/src/app/pages/Home.tsx
+++ shopland-fe/src/app/pages/Home.tsx
@@ -1,5 +1,5 @@
 import {useState, useEffect} from 'react';
-import {Container, Grid2, Paper, TextField, Typography} from "@mui/material";
+import {Box, Container, Grid2, Paper, TextField, Typography} from "@mui/material";
 
 interface Product {
     id: number;
@@ -31,13 +31,50 @@ export function Home() {
                 label="Wpisz coś"
                 variant="outlined"
                 fullWidth
+                sx={{ marginBottom: '1rem' }}
             />
             <Grid2 container spacing={1}>
                 {products.length > 0 ? (
                     products.map((product) => (
                         <Grid2 size={4} key={product.id}>
-                            <Paper elevation={3} sx={{padding: 2, textAlign: 'center'}}>
+                            <Paper elevation={3} sx={{padding: 2, textAlign: 'center', minHeight: 200}}>
                                 <Typography variant="h6">{product.name}</Typography>
+                                <Box
+                                    sx={{
+                                        width: '100%',
+                                        height: '80%',
+                                        backgroundColor: '#f0f0f0', // Kolor tła dla miejsca na zdjęcie
+                                        display: 'flex',
+                                        alignItems: 'center',
+                                        justifyContent: 'center',
+                                        borderRadius: '4px',
+                                        marginBottom: 2,
+                                        overflow: 'hidden',
+                                    }}
+                                >
+                                    {/*{product.image ? (*/}
+                                    {/*    <img*/}
+                                    {/*        src={product.image} // Link do zdjęcia*/}
+                                    {/*        alt={product.name}*/}
+                                    {/*        style={{*/}
+                                    {/*            maxWidth: '100%',*/}
+                                    {/*            maxHeight: '100%',*/}
+                                    {/*            objectFit: 'cover',*/}
+                                    {/*        }}*/}
+                                    {/*    />*/}
+                                    {/*) : (*/}
+                                    {/*    <Typography variant="caption" color="textSecondary">No photo</Typography>*/}
+                                    {/*)}*/}
+                                    <img
+                                        src="https://media.istockphoto.com/id/471212116/pl/zdj%C4%99cie/mops-gog.jpg?s=1024x1024&w=is&k=20&c=9o9ke6JIjAtxhg22ESuOtw0XRB-fP3fw4_RwUXDgRxk=" // Link do zdjęcia pieska
+                                        alt="Dog"
+                                        style={{
+                                            maxWidth: '100%',
+                                            maxHeight: '100%',
+                                            objectFit: 'cover',
+                                        }}
+                                    />
+                                </Box>
                                 <Typography variant="body2">{product.shortDescription}</Typography>
                                 <Typography variant="subtitle1" color="primary">
                                     {product.price.toFixed(2)} zł
