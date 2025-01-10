24671ebc38a29afc7be0d2f75cd6dd3e9ab809d5
Mateusz <mateusz.oracz2001@gmail.com>
2025-01-10 12:25:58 +0100

    basic view of home page improved

--- shopland-fe/src/app/pages/Home.tsx
+++ shopland-fe/src/app/pages/Home.tsx
@@ -6,6 +6,7 @@ interface Product {
     name: string;
     shortDescription: string;
     price: number;
+    imageUrl: string;
 }
 
 export function Home() {
@@ -35,15 +36,15 @@ export function Home() {
             />
             <Grid2 container spacing={1}>
                 {products.length > 0 ? (
-                    products.map((product) => (
+                    products.slice(0, 20).map((product) => (
                         <Grid2 size={4} key={product.id}>
                             <Paper elevation={3} sx={{padding: 2, textAlign: 'center', minHeight: 200}}>
                                 <Typography variant="h6">{product.name}</Typography>
                                 <Box
                                     sx={{
                                         width: '100%',
-                                        height: '80%',
-                                        backgroundColor: '#f0f0f0', // Kolor tła dla miejsca na zdjęcie
+                                        height: '250px',
+                                        backgroundColor: '#f0f0f0',
                                         display: 'flex',
                                         alignItems: 'center',
                                         justifyContent: 'center',
@@ -52,30 +53,20 @@ export function Home() {
                                         overflow: 'hidden',
                                     }}
                                 >
-                                    {/*{product.image ? (*/}
-                                    {/*    <img*/}
-                                    {/*        src={product.image} // Link do zdjęcia*/}
-                                    {/*        alt={product.name}*/}
-                                    {/*        style={{*/}
-                                    {/*            maxWidth: '100%',*/}
-                                    {/*            maxHeight: '100%',*/}
-                                    {/*            objectFit: 'cover',*/}
-                                    {/*        }}*/}
-                                    {/*    />*/}
-                                    {/*) : (*/}
-                                    {/*    <Typography variant="caption" color="textSecondary">No photo</Typography>*/}
-                                    {/*)}*/}
-                                    <img
-                                        src="https://media.istockphoto.com/id/471212116/pl/zdj%C4%99cie/mops-gog.jpg?s=1024x1024&w=is&k=20&c=9o9ke6JIjAtxhg22ESuOtw0XRB-fP3fw4_RwUXDgRxk=" // Link do zdjęcia pieska
-                                        alt="Dog"
-                                        style={{
-                                            maxWidth: '100%',
-                                            maxHeight: '100%',
-                                            objectFit: 'cover',
-                                        }}
-                                    />
+                                    {product.imageUrl ? (
+                                        <img
+                                            src={product.imageUrl}
+                                            alt={product.name}
+                                            style={{
+                                                maxWidth: '100%',
+                                                maxHeight: '100%',
+                                                objectFit: 'contain',
+                                            }}
+                                        />
+                                    ) : (
+                                        <Typography variant="caption" color="textSecondary">No photo</Typography>
+                                    )}
                                 </Box>
-                                <Typography variant="body2">{product.shortDescription}</Typography>
                                 <Typography variant="subtitle1" color="primary">
                                     {product.price.toFixed(2)} zł
                                 </Typography>
