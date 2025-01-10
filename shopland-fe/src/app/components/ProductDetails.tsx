edd21dd4c2732fd23f247e2174c28ef0a735342c
Mateusz <mateusz.oracz2001@gmail.com>
2025-01-10 22:15:27 +0100

    adding to cart button available

--- shopland-fe/src/app/components/ProductDetails.tsx
+++ shopland-fe/src/app/components/ProductDetails.tsx
@@ -1,7 +1,9 @@
 import {useEffect, useState} from 'react';
 import {useParams} from 'react-router-dom';
-import {Box, Typography, CircularProgress, Paper, Grid2, Button, Stack} from '@mui/material';
+import {Box, Button, CircularProgress, Paper, Stack, Typography, ListItem} from '@mui/material';
 import {OpinionCard} from './OpinionCard';
+import {useAuth} from "../context/AuthContext.tsx";
+import {Role} from "../models/User.ts";
 
 interface Product {
     name: string;
@@ -19,18 +21,13 @@ interface Opinion {
     value: number;
 }
 
-interface User {
-    isAuthenticated: boolean;
-    role: string;
-}
-
 export function ProductDetails() {
-    const {id} = useParams<{ id: string }>();
+    const { id } = useParams<{ id: string }>();
+    const { user } = useAuth();
     const [product, setProduct] = useState<Product | null>(null);
     const [opinions, setOpinions] = useState<Opinion[]>([]);
     const [loading, setLoading] = useState<boolean>(true);
     const [error, setError] = useState<string | null>(null);
-    const [user, setUser] = useState<User | null>(null);
 
     useEffect(() => {
         const fetchProduct = async () => {
@@ -45,7 +42,6 @@ export function ProductDetails() {
                 if (!opinionResponse.ok) throw new Error('Nie udało się pobrać opinii');
 
                 const opinionData = await opinionResponse.json();
-                console.log(opinionData);
                 setOpinions(opinionData);
             } catch (err: any) {
                 setError(err.message);
@@ -57,6 +53,36 @@ export function ProductDetails() {
         fetchProduct();
     }, [id]);
 
+    const addToCart = async () => {
+        const cartProductCommand = {
+            productId: Number(id),
+            quantity: 1,
+        };
+
+        setLoading(true);
+        try {
+            const response = await fetch('http://localhost:8080/shopland/rest/api/cart', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${user?.jwtToken}`,
+                },
+                body: JSON.stringify(cartProductCommand),
+            });
+
+            if (!response.ok) {
+                throw new Error('Nie udało się dodać produktu do koszyka');
+            }
+
+            const data = await response.json();
+            console.log('Produkt dodany do koszyka', data);
+        } catch (error: any) {
+            setError(error.message);
+        } finally {
+            setLoading(false);
+        }
+    };
+
     const handleDeleteOpinion = async (opinionId: number) => {
         try {
             const response = await fetch(`http://localhost:8080/shopland/rest/api/opinion/products/${id}/${opinionId}`, {
@@ -67,11 +93,12 @@ export function ProductDetails() {
                 throw new Error('Nie udało się usunąć opinii');
             }
 
-            setOpinions(opinions.filter(opinion => opinion.id !== opinionId));
+            setOpinions(prevOpinions => prevOpinions.filter(opinion => opinion.id !== opinionId));
         } catch (err: any) {
             setError('Błąd podczas usuwania opinii');
         }
     };
+
 
     const calculateAverageRating = (opinions: Opinion[]) => {
         if (opinions.length === 0) return 0;
@@ -80,7 +107,7 @@ export function ProductDetails() {
     };
 
     if (loading) {
-        return <CircularProgress/>;
+        return <CircularProgress />;
     }
 
     if (error) {
@@ -92,8 +119,8 @@ export function ProductDetails() {
     }
 
     return (
-        <Box sx={{display: 'flex', flexDirection: 'column', padding: 2, gap: 3, marginTop: '3rem'}}>
-            <Paper elevation={3} sx={{padding: 2, display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+        <Box sx={{ display: 'flex', flexDirection: 'column', padding: 2, gap: 3, marginTop: '3rem' }}>
+            <Paper elevation={3} sx={{ padding: 2, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
                 <Typography variant="h4">{product.name}</Typography>
                 {product.imageUrl && (
                     <Box
@@ -112,24 +139,25 @@ export function ProductDetails() {
                         <img
                             src={product.imageUrl}
                             alt={product.name}
-                            style={{maxWidth: '100%', maxHeight: '100%', objectFit: 'contain'}}
+                            style={{ maxWidth: '100%', maxHeight: '100%', objectFit: 'contain' }}
                         />
                     </Box>
                 )}
-                <Typography variant="subtitle1" color="textSecondary" sx={{textAlign: 'center'}}>
+                <Typography variant="subtitle1" color="textSecondary" sx={{ textAlign: 'center' }}>
                     {product.description || 'Brak opisu'}
                 </Typography>
-                <Typography variant="h6" color="primary" sx={{marginTop: 2}}>
+                <Typography variant="h6" color="primary" sx={{ marginTop: 2 }}>
                     Dostępna ilość: {product.availableAmount}
                 </Typography>
 
                 <Button
                     variant="contained"
                     color="primary"
-                    sx={{marginTop: 2}}
-                    disabled={!user?.isAuthenticated || product.availableAmount <= 0}
+                    sx={{ marginTop: 2 }}
+                    disabled={!(user?.hasRole(Role.USER) || user?.hasRole(Role.ADMIN)) || product.availableAmount <= 0}
+                    onClick={addToCart}
                 >
-                    {user?.isAuthenticated ? 'Dodaj do koszyka' : 'Zaloguj się, aby dodać do koszyka'}
+                    {(user?.hasRole(Role.USER) || user?.hasRole(Role.ADMIN)) ? 'Dodaj do koszyka' : 'Zaloguj się, aby dodać do koszyka'}
                 </Button>
             </Paper>
 
@@ -137,25 +165,25 @@ export function ProductDetails() {
 
             <Typography variant="h5">Opinie:</Typography>
             {opinions.length > 0 ? (
-                <Stack spacing={2}
-                       alignItems="center"
-                       justifyContent="center"
-                    >
-                    {opinions.slice(0, 10).map((opinion) => (
+                <Stack spacing={2}>
+                    {opinions.map((opinion) => (
+                        <ListItem key={opinion.id} sx={{padding: 0, display: 'flex', flexDirection: 'column'}}>
                             <OpinionCard
                                 opinion={opinion}
-                                usernameVisible={true}
-                                shouldDeleteBeVisible={user?.role === 'ADMIN'}
-                                shouldEditBeVisible={false}
+                                usernameVisible={false}
+                                shouldDeleteBeVisible={user?.role === Role.ADMIN}
+                                shouldEditBeVisible={user?.role === Role.ADMIN}
                                 onDeleteOpinion={handleDeleteOpinion}
                             />
+                            <div style={{borderBottom: '1px solid #ccc', margin: '10px 0'}}/>
+                        </ListItem>
                     ))}
                 </Stack>
             ) : (
                 <Typography>Brak opinii o tym produkcie.</Typography>
             )}
 
-            {user?.isAuthenticated && (
+            {(user?.hasRole(Role.ADMIN) || user?.hasRole(Role.ADMIN)) && (
                 <Button
                     variant="contained"
                     color="secondary"
